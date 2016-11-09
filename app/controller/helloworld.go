95f835f177504cb0f652b7af3e4568de250de180
jia <jiada8866@126.com>
2016-11-09 11:01:38 +0800

    add more mock api

--- app/controller/helloworld.go
+++ app/controller/helloworld.go
@@ -15,6 +15,20 @@ func Hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
 
+func One(c echo.Context) error {
+
+	randomErr()
+
+	return c.String(http.StatusOK, "One")
+}
+
+func Two(c echo.Context) error {
+
+	randomErr()
+
+	return c.String(http.StatusOK, "Two")
+}
+
 func randomErr() {
 	rand.Seed(time.Now().Unix())
 	if rand.Intn(2) == 0 {
