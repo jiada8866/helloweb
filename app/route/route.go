6278451f5324e35363e88fc083984977f1b3551c
jia <jiada8866@126.com>
2016-11-09 11:01:52 +0800

    add more mock api

--- app/route/route.go
+++ app/route/route.go
@@ -7,6 +7,8 @@ import (
 
 func AddRouters(e *echo.Echo) {
 	e.GET("/", controller.Hello)
+	e.GET("/one", controller.One)
+	e.GET("/two", controller.Two)
 
 	e.POST("/post", controller.GetPostData)
 
