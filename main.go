c56afb5d9f0638656e1dbe3d1a3f68dcb1585202
jia <jiada8866@126.com>
2016-11-20 10:12:40 +0800

    use logrus as echo's logger

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	log "github.com/Sirupsen/logrus"
 	"github.com/jiada8866/helloweb/app/route"
 	"github.com/jiada8866/helloweb/app/route/middleware/echologrus"
 	"github.com/jiada8866/helloweb/app/shared/logger"
@@ -23,6 +24,15 @@ func main() {
 	logger.Init(logfile, false)
 
 	e := echo.New()
+
+	e.SetDebug(true)
+
+	// Logger as an io.Writer
+	// 可能是最简单的用logrus打印echo自身日志的方式
+	// 缺点：logrus打印的echo日志的level都是info，而且将echo日志都写在msg里
+	w := log.StandardLogger().Writer()
+	defer w.Close()
+	e.SetLogOutput(w)
 
 	e.Use(echologrus.New())
 	e.Use(middleware.Recover())
